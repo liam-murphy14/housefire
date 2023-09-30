ae76f9a1dea2d23bbc19cd4cedc1dcaf0fae52f7
liam-murphy14 <liam.murphy137@gmail.com>
2023-09-30 13:46:50 -0700

    seo tag working well

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
-<script>
+<script lang="ts">
   import '../app.postcss';
+  import Seo from '$lib/components/Seo.svelte';
+  import { page } from '$app/stores';
 </script>
 
 <svelte:head>
@@ -8,6 +10,8 @@
     href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900"
   />
 </svelte:head>
+
+<Seo title={$page.data.metaTags.title} metadescription={$page.data.metaTags.description} />
 
 <div class="h-screen w-screen -z-50 bg-hf-base-light overflow-auto flex flex-col">
   <div class="sticky top-0 p-2 w-full border-b border-hf-grey bg-hf-base-light flex-shrink-0">
