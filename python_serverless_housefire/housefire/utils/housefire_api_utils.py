c0bdc4ab36b37b55b6b14aaae37a67b6f1bafb20
liam-murphy14 <liam.murphy137@gmail.com>
2024-05-19 12:29:01 -0700

    fix endpoints

--- python_serverless_housefire/housefire/utils/housefire_api_utils.py
+++ python_serverless_housefire/housefire/utils/housefire_api_utils.py
@@ -10,21 +10,21 @@ class HousefireAPI:
             "Content-Type": "application/json",
         }
 
-    def __clean_endpoint(self, endpoint: str):
+    def __construct_url(self, endpoint: str):
         if endpoint.startswith("/") and len(endpoint) > 1:
-            return endpoint[1:]
-        return endpoint
+            return self.base_url + endpoint[1:]
+        return self.base_url + endpoint
 
     def __get(self, endpoint, params=None) -> r.Response:
         return r.get(
-            self.__clean_endpoint(endpoint), headers=self.headers, params=params
+            self.__construct_url(endpoint), headers=self.headers, params=params
         )
 
     def __post(self, endpoint, data=None) -> r.Response:
-        return r.post(self.__clean_endpoint(endpoint), headers=self.headers, json=data)
+        return r.post(self.__construct_url(endpoint), headers=self.headers, json=data)
 
     def __delete(self, endpoint) -> r.Response:
-        return r.delete(self.__clean_endpoint(endpoint), headers=self.headers)
+        return r.delete(self.__construct_url(endpoint), headers=self.headers)
 
     def get_properties_by_ticker(self, ticker: str) -> r.Response:
         return self.__get(f"/properties/{ticker}")
