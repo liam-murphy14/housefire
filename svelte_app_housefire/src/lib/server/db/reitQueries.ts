2ef07ca9d3d55506f58df4c7d836bb9b73c4dd90
liam-murphy14 <liam.murphy137@gmail.com>
2024-09-29 11:46:52 -0700

    feat: add reit create endpoint

--- svelte_app_housefire/src/lib/server/db/reitQueries.ts
+++ svelte_app_housefire/src/lib/server/db/reitQueries.ts
@@ -1,5 +1,6 @@
 import prisma from '$lib/server/db/prisma';
 import type { Reit } from '@prisma/client';
+import type { Prisma } from '@prisma/client';
 
 export const getAllReits: () => Promise<Reit[]> = async () => {
   return await prisma.reit.findMany();
@@ -8,4 +9,10 @@ export const getAllReits: () => Promise<Reit[]> = async () => {
 export const getAllTickers: () => Promise<string[]> = async () => {
   const reits = await getAllReits();
   return reits.map((reit) => reit.ticker);
+};
+
+export const createReit: (reit: Prisma.ReitCreateInput) => Promise<Reit> = async (reitToCreate) => {
+  return await prisma.reit.create({
+    data: reitToCreate,
+  });
 };
---
+++ svelte_app_housefire/src/routes/api/reits/+server.ts
@@ -0,0 +1,25 @@
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { ReitCreateInputSchema } from '$lib/utils/prismaGeneratedZod';
+import { ZodError } from 'zod';
+import { createReit } from '$lib/server/db/reitQueries';
+
+export const POST: RequestHandler = async ({ request }) => {
+  try {
+    const body = await request.json();
+    console.log('received POST request to /api/reits: ', body);
+    const reitCreateInput = ReitCreateInputSchema.parse(body);
+    const reitCreatePrismaResponse = await createReit(reitCreateInput);
+    return json(reitCreatePrismaResponse);
+  } catch (e) {
+    if (e instanceof ZodError) {
+      const zodErrorMessages = e.errors.map((error) => error.message).join(', ');
+      error(400, {
+        message: zodErrorMessages,
+      });
+    }
+    error(500, {
+      message: 'Something went wrong',
+    });
+  }
+};
